201f1bb9ab979ea02999556d40d28912b6bc590c
Felipe Fernandes <felipegabrielsouzafernandes@gmail.com>
2026-04-06 18:54:57 -0300

    commit

--- lista02/ex15/main.go
+++ lista02/ex15/main.go
@@ -9,7 +9,7 @@ func main() {
 	meses := []string{"", "janeiro", "fevereiro", "março", "abril", "maio", "junho", "julho", "agosto", "setembro", "outubro", "novembro", "dezembro"}
 
 	if mes >= 1 && mes <= 12 {
-		fmt.Printf("%d de %s de %d\n", dia, months[mes], ano)
+		fmt.Printf("%d de %s de %d\n", dia, meses[mes], ano)
 	} else {
 		fmt.Println("Mês inválido")
 	}
--- lista02/ex19/main.go
+++ lista02/ex19/main.go
@@ -1,8 +1,10 @@
 package main
+
 import (
 	"fmt"
 	"math"
 )
+
 func main() {
 	pi := math.Pi
 	raio := 0.0
@@ -22,35 +24,17 @@ func main() {
 		volume = pi * (raio * raio) * altura
 		fmt.Printf("a area do cilindro é: %v", area)
 		fmt.Printf("o volume do cilindro é: %v", volume)
-		case "cone":
-		area = pi * raio * (math.Sqrt((raio*raio)+(altura*altura)))
+	case "cone":
+		area = pi * raio * (math.Sqrt((raio * raio) + (altura * altura)))
 
 		volume = (1.0 / 3.0) * pi * (raio * raio) * altura
 
 		fmt.Printf("a area do cone é: %v", area)
 		fmt.Printf("o volume do cone é: %v", volume)
-		case "esfera":
+	case "esfera":
 		area = 4 * pi * (raio * raio)
 		volume = (4.0 / 3.0) * pi * (raio * raio * raio)
 		fmt.Printf("a area da esfera é: %v", area)
 		fmt.Printf("o volume da esfera é: %v", volume)
 	}
-}
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 }
