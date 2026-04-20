3c501f819e4d5a336bbe540dc1487334aa6ea26b
Felipe Fernandes <felipegabrielsouzafernandes@gmail.com>
2026-04-20 15:56:02 -0300

    organização

--- lista03/ex27/main.go
+++ lista03/ex27/main.go
@@ -1,13 +1,15 @@
-ackage main
+package main
 
-import (f "fmt"
-		m "math")
+import (
+	f "fmt"
+	m "math"
+)
 
-func fatorial(n int)int {
-     var P int = 1
-      
-	for i := 1; i <= n; i++{
-          P = P*i
+func fatorial(n int) int {
+	var P int = 1
+
+	for i := 1; i <= n; i++ {
+		P = P * i
 	}
 	return P
 }
@@ -19,8 +21,8 @@ func main() {
 
 	y := 1.0
 
-	for i:=1; i<=19;i++{
-		y = y + float64(m.Pow(x,float64(i+1)))*float64(m.Pow(-1, float64(i))/float64(fatorial(2*i)))
+	for i := 1; i <= 19; i++ {
+		y = y + float64(m.Pow(x, float64(i+1)))*float64(m.Pow(-1, float64(i))/float64(fatorial(2*i)))
 	}
-	f.Println("Cosseno(x)=",y)
+	f.Println("Cosseno(x)=", y)
 }
